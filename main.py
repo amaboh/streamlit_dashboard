1cdf3fe7775749a8236d264f26ba384574ef5835
Ama <85511496+amaboh@users.noreply.github.com>
2023-12-03 15:00:38 +0000

    time series diagram, treemap and pie chart for sales by segments and category

--- main.py
+++ main.py
@@ -70,10 +70,79 @@ elif not region and not city:
 elif state and city:
     filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
 elif region and city:
-    filtered_df = df3[df["State"].isin(region) & df3["City"].isin(city)]
+    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
 elif region and state:
-    filtered_df = df3[df["State"].isin(region) & df3["City"].isin(state)]
+    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
 elif city:
     filtered_df = df3[df3["City"].isin(city)]
 else:
-    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(city)]
+    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
+    
+category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
+
+with col1:
+    st.subheader("Sales by Category")
+    fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in category_df["Sales"]],
+                 template = "seaborn")
+    st.plotly_chart(fig, use_container_width=True, height = 200)
+    
+with col2:
+    st.subheader("Sales by Region")
+    fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
+    fig.update_traces(text =filtered_df["Region"], textposition = "outside")
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# download data by category and region button
+cl1, cl2 = st.columns((2))
+with cl1:
+    with st.expander("Category_ViewData"):
+        st.write(category_df.style.background_gradient(cmap="Blues"))
+        csv = category_df.to_csv(index = False).encode('utf-8')
+        st.download_button("Download Data", data = csv, file_name = "Category.csv", mime = "text/csv",
+                           help = "Click here to download the data as a CSV file")
+        
+with cl2:
+    with st.expander("Region_ViewData"):
+        region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
+        st.write(region.style.background_gradient(cmap="Oranges"))
+        csv = region.to_csv(index = False).encode('utf-8')
+        st.download_button("Download Data", data = csv, file_name = "Region.csv", mime= "text/csv",
+                        help = " Click to download the data as CSV file")
+
+
+filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+st.subheader(' Time Series Analysis')
+
+linechart = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+fig2 = px.line(linechart, x = "month_year", y="Sales", labels = {"Sales" : "Amount"}, height=500, width=1000, template="gridon")
+st.plotly_chart(fig2, use_container_width=True)    
+
+with st.expander("View Data of TimeSeries:"):
+    st.write(linechart.T.style.background_gradient(cmap="Blues"))
+    csv = linechart.to_csv(index=False).encode("utf-8")
+    st.download_button("Download Data", data = csv, file_name = "TimeSeries.csv", mime='text/csv')
+    
+# Tree diagram for region, category, sub-category
+st.subheader("Treemap of Sales")
+fig3 = px.treemap(filtered_df, path = ["Region", "Category", "Sub-Category"], values = "Sales", hover_data= ["Sales"],
+                  color = "Sub-Category")
+
+fig3.update_layout(width = 800, height = 650)
+st.plotly_chart(fig3, use_container_width=True)
+
+
+chart1, chart2 = st.columns((2))
+
+with chart1:
+    st.subheader('Sales by Segments')
+    fig = px.pie(filtered_df, values = "Sales", names="Segment", template = "plotly_dark")
+    fig.update_traces(text = filtered_df["Segment"], textposition = "inside")
+    st.plotly_chart(fig, use_container_width=True)
+    
+with chart2:
+    st.subheader('Sales by Category')
+    fig = px.pie(filtered_df, values = "Sales", names="Category", template = 'gridon')
+    fig.update_traces(text = filtered_df["Category"], textposition= "inside")
+    st.plotly_chart(fig, use_container_width=True)
+    
